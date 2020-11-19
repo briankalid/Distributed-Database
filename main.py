819cd25b23a5b550e93793d79666ddb0ad23e145
jruiz971 <juanluisruiz971@gamil.com>
2020-11-19 15:55:25 -0600

    menu

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ This proyect is under GPL-3.0 License
 import database
 import processing
 
-cnx_morelia=database.morelia()
-cnx_patzcuaro=database.patzcuaro()
+#cnx_morelia=database.morelia()
+#cnx_patzcuaro=database.patzcuaro()
 
-print(processing.prueba(cnx_morelia))
+#print(processing.prueba(cnx_morelia))
+
+
+#--- Menu ---#
+#Mostrar sucursales
+print ('Selecciona tu sucursal')
+sucursal = int(input())
+print('\n\t Bienvenido \n\t Opciones: \n\t\t Insertar nuevos clientes')

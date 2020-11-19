5efa21d67e42eeec66be2e2960b1388801ca2576
briankalid <briankalid2000@gmail.com>
2020-11-19 13:59:27 -0600

    sucursal required

--- processing.py
+++ processing.py
@@ -6,7 +6,7 @@ def prueba(cnx):
     cursor.execute(query)
     return(cursor.fetchall())
 
-def inDatabase(cnxm,cnxp,nombre,ap,am,rfc):
+def inDatabase(sucursal,cnxm,cnxp,nombre,ap,am,rfc):
 #    existin_morelia=False
  #   existin_patz=False
 
@@ -20,12 +20,16 @@ def inDatabase(cnxm,cnxp,nombre,ap,am,rfc):
    
     if len(cursor.fetchall())>0:
         #existin_morelia=True
+        print('Ya existe en la base de datos')
+        print('Los siguientes datos son correctos?')
         print(cursor.fetchall())
         return True
     
     elif len(cursorp.fetchall())>0:
-        #existin_patz=True
+        print('Ya existe en la base de datos')
+        print('Los siguientes datos son correctos?')
         print(cursorp.fetchall())
+
         
         return True
 

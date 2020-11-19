aa6910b0267210e511495ef27ad2087362f13845
jruiz971 <juanluisruiz971@gamil.com>
2020-11-19 15:07:50 -0600

    Checking

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+'''
+This proyect is under GPL-3.0 License
+
+@Authors:
+	Juan Luis Ruiz Vanegas    ->  juanluisruiz971@gmail.com
+	Brian Kalid Garcia Olivo  ->  briankalid2000@gmail.com
+'''
+
 import database
 import processing
 

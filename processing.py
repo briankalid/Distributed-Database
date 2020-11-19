1fb9e0add57e7f268fe9c90323f6e5370dc46f5d
briankalid <briankalid2000@gmail.com>
2020-11-19 15:12:53 -0600

    registro v0.1

--- processing.py
+++ processing.py
@@ -1,4 +1,18 @@
 import mysql.connector
+import random
+
+
+def generate_id(UG):
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    ID = UG
+    for x in range(7):
+        if (x%2 == 0):
+            ID += str(random.randint(0,9))
+        else:
+            ID += random.choice(letters)
+
+    return(ID)
+
 
 def prueba(cnx):
     cursor=cnx.cursor()
@@ -6,36 +20,48 @@ def prueba(cnx):
     cursor.execute(query)
     return(cursor.fetchall())
 
-def inDatabase(sucursal,cnxm,cnxp,nombre,ap,am,rfc):
+def inDatabase(cnxm,cnxp,nombre,ap,am,rfc):
 #    existin_morelia=False
  #   existin_patz=False
 
     cursor=cnxm.cursor()
     query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
     cursor.execute(query,(nombre,ap,am,rfc))
+    resm=cursor.fetchall()
 
     cursorp=cnxp.cursor()
     query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
     cursorp.execute(query,(nombre,ap,am,rfc))
-   
-    if len(cursor.fetchall())>0:
+    resp=cursorp.fetchall()
+
+    if len(resm)>0:
         #existin_morelia=True
         print('Ya existe en la base de datos')
         print('Los siguientes datos son correctos?')
-        print(cursor.fetchall())
+        print(resm)
         return True
     
-    elif len(cursorp.fetchall())>0:
+    elif len(resp)>0:
         print('Ya existe en la base de datos')
         print('Los siguientes datos son correctos?')
-        print(cursorp.fetchall())
+        print(resp)
 
         
         return True
 
     
     else:
-        cursor.close()
-        cursorp.close()
+        #cursor.close()
+        #cursorp.close()
         return False
+
+
+def registrar_cliente(cnx,nombre,ap,am,rfc):
+    cursor = cnx.cursor()
+    id = generate_id("U")
+    query = """INSERT INTO Clientes(Id,Nombre,Apellido_Paterno,Apellido_Materno,RFC) VALUES (%s,%s,%s,%s,%s)"""
+    data_query = (id,nombre,ap,am,rfc)
+    cursor.execute(query,data_query)
+    cnx.commit()
+    print("Query efectuadas correctamente...")
 

95eeef8d39dc83595055819d4981a441faa3a238
jruiz971 <juanluisruiz971@gamil.com>
2020-11-20 19:29:46 -0600

    menu

--- main.py
+++ main.py
@@ -16,7 +16,34 @@ import processing
 
 
 #--- Menu ---#
-#Mostrar sucursales
-print ('Selecciona tu sucursal')
-sucursal = int(input())
-print('\n\t Bienvenido \n\t Opciones: \n\t\t Insertar nuevos clientes')
+def sucursal():
+	#Mostrar sucursales
+	print ('\n\t Bienvenido \n\t Selecciona tu sucursal \n\t 1.- Morelia \n\t 2.-Patzcuaro ')
+
+	try:
+		sucursal =  int(input())
+		if (sucursal<1 or sucursal>2):
+			print('Opcion invalida')
+		else: return ( sucursal )
+	except:
+		print('Opcion invalida'
+
+
+def operacion():
+	print('\n\t \n\t Opciones: \n\t\t 1.- Insertar nuevos clientes y direcciones \n\t\t')
+	print('\n\t\t 2.- Actualizar datos de los clientes y direcciones \n\t\t')
+	print('\n\t\t 3.- Buscar Clientes por nombre, RFC, o domicilio desde cualquier sucursal \n\t\t')
+
+	try:
+		operacion = int(input())
+		if (operacion<1 or operacion>3):
+			print('Opcion invalida')
+		else: return ( operacion )
+	except:
+		print('Opcion invalida')
+	
+
+sucursal = sucursal()
+operacion =  operacion()
+
+	

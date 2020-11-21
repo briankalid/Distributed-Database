5f24f3ab0d2e3a313835babc1eb3099b350de1c4
briankalid <briankalid2000@gmail.com>
2020-11-20 20:48:44 -0600

    revisa las bases de datos en busca del cliente

--- processing.py
+++ processing.py
@@ -20,45 +20,63 @@ def prueba(cnx):
     cursor.execute(query)
     return(cursor.fetchall())
 
-def inDatabase(cnxm,cnxp,nombre,ap,am,rfc):
+def inDatabase(sucursales,cnxs,nombre,ap,am,rfc):
+    for i,cnx in enumerate(cnxs):
+        cursor=cnx.cursor()
+        query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
+        cursor.execute(query,(nombre,ap,am,rfc))
+        resp=cursor.fetchall()
+        
+        if(len(resp))>0:
+            print('Ya existe en la base de datos de',sucursales[i])
+            print('Los siguientes datos son correctos?')
+            
+            query="""SELECT * FROM Direcciones Where Id_Cliente=%s"""
+            cursor.execute(query,(resp[0][0],))
+            print(cursor.fetchall())
+ 
+            return True
+
+
+
 #    existin_morelia=False
  #   existin_patz=False
 
-    cursor=cnxm.cursor()
-    query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
-    cursor.execute(query,(nombre,ap,am,rfc))
-    resm=cursor.fetchall()
+# cursor=cnxm.cursor()
+# query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
+# cursor.execute(query,(nombre,ap,am,rfc))
+# resm=cursor.fetchall()
 
-    cursorp=cnxp.cursor()
-    query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
-    cursorp.execute(query,(nombre,ap,am,rfc))
-    resp=cursorp.fetchall()
-    print(resp)
-    if len(resm)>0:
-        #existin_morelia=True
-        print('Ya existe en la base de datos')
-        print('Los siguientes datos son correctos?')
-        query="""SELECT * FROM Direcciones Where Id_Cliente=%s"""
-        cursor.execute(query,(resm[0][0],))
-        print(cursor.fetchall())
+# cursorp=cnxp.cursor()
+# query="""SELECT * FROM Clientes Where Nombre=%s and Apellido_Paterno=%s and Apellido_Materno=%s and RFC=%s"""
+# cursorp.execute(query,(nombre,ap,am,rfc))
+# resp=cursorp.fetchall()
+# print(resp)
+# if len(resm)>0:
+#     #existin_morelia=True
+#     print('Ya existe en la base de datos')
+#     print('Los siguientes datos son correctos?')
+#     query="""SELECT * FROM Direcciones Where Id_Cliente=%s"""
+#     cursor.execute(query,(resm[0][0],))
+#     print(cursor.fetchall())
 
-        return True
-    
-    elif len(resp)>0:
-        print('Ya existe en la base de datos')
-        print('Los siguientes datos son correctos?')
-        query="""SELECT * FROM Direcciones Where Id_Cliente=%s"""
-        cursorp.execute(query,(resp[0][0],))
-        print(cursorp.fetchall())
+#     return True
+# 
+# elif len(resp)>0:
+#     print('Ya existe en la base de datos')
+#     print('Los siguientes datos son correctos?')
+#     query="""SELECT * FROM Direcciones Where Id_Cliente=%s"""
+#     cursorp.execute(query,(resp[0][0],))
+#     print(cursorp.fetchall())
 
-        
-        return True
+#     
+#     return True
 
-    
-    else:
-        #cursor.close()
-        #cursorp.close()
-        return False
+# 
+# else:
+#     #cursor.close()
+#     #cursorp.close()
+#     return False
 
 
 def registrar_cliente(cnx,nombre,ap,am,rfc,calle,col,est,cp):

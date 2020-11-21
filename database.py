957ab6993ae1cb2aabfe4c9da9a7d1c23d779bbb
briankalid <briankalid2000@gmail.com>
2020-11-20 19:06:14 -0600

    mulidatabases open

--- database.py
+++ database.py
@@ -2,49 +2,32 @@ import json
 import mysql.connector
 from mysql.connector import errorcode
 
-def morelia():
-    with open('dbmorelia.json') as json_file:
-        configmore = json.load(json_file)
-        json_file.close()
+def init_databases(dtbs):
+    cnxs=[]
+    for i,db in enumerate(dtbs):
+        file = 'db'+db+'.json'
+        print(file)
+        with open(file) as json_file:
+            config = json.load(json_file)
+            json_file.close()
 
 
-    try:
-        cnxm = mysql.connector.connect(**configmore)
-        print('\n'+'\033[0;32m'+'Conexion a la base de datos de Morelia exitosa'+'\033[0;m')
-        #cursorm = cnxm.cursor()
-        return cnxm
-        
+        try:
+            cnx = mysql.connector.connect(**config)
+            print('\n'+'\033[0;32m'+'Conexion a la base de datos de',db,'exitosa'+'\033[0;m')
+            cnxs.append(cnx)
+            #cursorp = cnxp.cursor()
+            #return cnxp
 
-    except mysql.connector.Error as err:
-            print('Morelia:')
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+        except mysql.connector.Error as err:
+                print(db)
+                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                    print("Something is wrong with your user name or password")
+                elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                    print("Database does not exist")
+                else:
+                    print(err)
 
-def patzcuaro():
-
-    with open('dbpatzcuaro.json') as json_file:
-        configpatz = json.load(json_file)
-        json_file.close()
-
-
-    try:
-        cnxp = mysql.connector.connect(**configpatz)
-        print('\n'+'\033[0;32m'+'Conexion a la base de datos de Patzcuaro exitosa'+'\033[0;m')
-        #cursorp = cnxp.cursor()
-        return cnxp
-
-    except mysql.connector.Error as err:
-            print('Patzcuaro:')
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
+    return cnxs
 
 
